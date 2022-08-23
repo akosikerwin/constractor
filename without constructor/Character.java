45386057458fab034e96bb5c275e646a9d05ca52
akosikerwin <111828131+akosikerwin@users.noreply.github.com>
2022-08-23 11:32:29 +0800

    Create Character.java

---
+++ without constructor/Character.java
@@ -0,0 +1,30 @@
+public class Character {
+    // Attributes
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+
+    // Method
+    public void sayMyName() {
+        System.out.println("Hello I am " + name);
+    }
+
+     // Method
+     public void sayMyStrength() {
+        System.out.println("My Strength is " + strength);
+    }
+        // Method
+     public void sayMyAgility() {
+        System.out.println("Say My Agility " + agility);
+    }
+        // Method
+     public void sayMyIntelligence() {
+        System.out.println("Say My Intelligence " + intelligence);
+
+
+
+    }
+
+
+}
